6b1160e26781b24f6486c68311ea8cecc403d863
Eduardo Hernandez Ley <eduardo.hley@gmail.com>
2021-09-21 11:55:44 -0500

    host fix

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
-require('dotenv').config()
-const fastify = require('fastify')()
-const fastifyCron = require('fastify-cron')
-const axios = require('axios');
-const { defaultError } = require('./schemas/shared');
-const { createDefaultEnv } = require('./util/env');
-const { getAdminAccessToken } = require('./routes/admin');
+require("dotenv").config();
+const fastify = require("fastify")();
+const fastifyCron = require("fastify-cron");
+const axios = require("axios");
+const { defaultError } = require("./schemas/shared");
+const { createDefaultEnv } = require("./util/env");
+const { getAdminAccessToken } = require("./routes/admin");
 
-fastify.register(require('fastify-cors'), {
-  origin: '*'
-})
+fastify.register(require("fastify-cors"), {
+  origin: "*",
+});
 
-fastify.register(require('./routes/admin').AdminRoutes);
-fastify.register(require('./routes/user').UserRoutes);
-fastify.register(require('./routes/role').RoleRoutes);
+fastify.register(require("./routes/admin").AdminRoutes);
+fastify.register(require("./routes/user").UserRoutes);
+fastify.register(require("./routes/role").RoleRoutes);
 
-fastify.addSchema(defaultError)
+fastify.addSchema(defaultError);
 
 fastify.register(fastifyCron, {
   jobs: [
     {
-      cronTime: '*/30 * * * * *',
+      cronTime: "*/30 * * * * *",
       onTick: async (server) => {
-        await getAdminAccessToken()
+        await getAdminAccessToken();
       },
     },
   ],
-})
+});
 
 const start = async () => {
   try {
-    createDefaultEnv()
-    await fastify.listen(process.env.PORT)
-    fastify.cron.startAllJobs()
-    await getAdminAccessToken()
+    createDefaultEnv();
+    fastify.listen(process.env.PORT, "0.0.0.0", async function (err, address) {
+      console.log(`Listening on ${address}`);
+      fastify.cron.startAllJobs();
+      await getAdminAccessToken();
+    });
   } catch (err) {
     console.log(err);
-    process.exit(1)
+    process.exit(1);
   }
-}
+};
 
-start()
+start();
--- util/env.js
+++ util/env.js
@@ -1,22 +1,22 @@
-const { existsSync, createWriteStream, writeFileSync, appendFileSync } = require('fs')
-const { join } = require('path')
+const { existsSync, createWriteStream, writeFileSync, appendFileSync } = require("fs");
+const { join } = require("path");
 
 module.exports.createDefaultEnv = () => {
-  if (!existsSync('.env')) {
-    const logger = createWriteStream('.env', {
-      flags: 'a'
-    })
+  if (!existsSync(".env")) {
+    const logger = createWriteStream(".env", {
+      flags: "a",
+    });
 
-    logger.write('PORT=8081\n')
-    logger.write('KC_SERVER=http://localhost:8080\n')
-    logger.write('KC_USERNAME=username\n')
-    logger.write('KC_PASSWORD=password\n')
-    logger.write('KC_REALM=realm\n')
-    logger.write('KC_CLIENT_ID=client\n')
-    logger.write('KC_ADMIN_CLI_ID=admin-cli')
+    logger.write("PORT=8181\n");
+    logger.write("KC_SERVER=http://localhost:8180\n");
+    logger.write("KC_USERNAME=username\n");
+    logger.write("KC_PASSWORD=password\n");
+    logger.write("KC_REALM=realm\n");
+    logger.write("KC_CLIENT_ID=client\n");
+    logger.write("KC_ADMIN_CLI_ID=admin-cli");
 
-    logger.end()
+    logger.end();
 
-    console.log('.env file created. Restart server.');
+    console.log(".env file created. Restart server.");
   }
-}
+};
